ac80c6da1d6be5d1b87fe52a36737d68f4a4694b
姚林 <yaolin1@xiaomi.com>
2019-06-28 15:38:34 +0800

    重置表单

--- src/router.js
+++ src/router.js
@@ -2,6 +2,7 @@ import Vue from "vue";
 import Router from "vue-router";
 import Home from "./views/Home.vue";
 import index from "./views/index.vue";
+import form from "./views/form.vue";
 
 Vue.use(Router);
 
@@ -15,6 +16,10 @@ export default new Router({
     {
       path: "/index",
       component: index
+    },
+    {
+      path: "/form",
+      component: form
     },
     {
       path: "/about",
---
+++ src/views/form.vue
@@ -0,0 +1,38 @@
+<template>
+  <el-form label-width="80px" :model="form">
+    <el-form-item label="名称">
+      <el-input v-model="form.name"></el-input>
+    </el-form-item>
+    <el-form-item label="活动区域">
+      <el-input v-model="form.region"></el-input>
+    </el-form-item>
+    <el-form-item label="活动形式">
+      <el-input v-model="form.type"></el-input>
+    </el-form-item>
+    <el-form-item>
+      <el-button @click="reset">重置</el-button>
+    </el-form-item>
+  </el-form>
+</template>
+
+<script>
+function createEmpty() {
+  return {
+    name: "",
+    region: "",
+    type: ""
+  };
+}
+export default {
+  data() {
+    return {
+      form: createEmpty()
+    };
+  },
+  methods: {
+    reset() {
+      this.form = createEmpty();
+    }
+  }
+};
+</script>
